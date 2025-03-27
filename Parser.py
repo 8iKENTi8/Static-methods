55f736b955b5372d3c6ae51aebd22cdf6912b74a
Вова Ролдугин <v.roldugin@owen.ru>
2025-03-27 18:27:35 +0300

    Фильтр вторичка, максимальный этаж в здании 50 и макс год 2022

--- Parser.py
+++ Parser.py
@@ -1,3 +1,5 @@
+# https://github.com/lenarsaitov/cianparser
+
 import cianparser
 import openpyxl
 
@@ -6,16 +8,18 @@ moscow_parser = cianparser.CianParser(location="Москва")
 
 # Собираем данные (~500 записей) с фильтрами для этажей квартиры и здания, а также с увеличенным лимитом по цене
 data = moscow_parser.get_flats(
-    deal_type="sale", 
-    rooms=(1, 2, 3, 4, 5),  
-    with_saving_csv=True,  
+    deal_type="sale",
+    rooms=(1, 2, 3, 4, 5),
+    with_saving_csv=True,
     additional_settings={
         "start_page": 1,
         "end_page": 20,
         "max_floor": 45,  # Ограничение по максимальному этажу квартиры
-        "max_building_floor": 50,  # Ограничение по максимальному этажу здания
+        "max_total_floor": 50,  # Ограничение по максимальному этажу здания
         "max_price": 200_000_000,  # Ограничение по максимальной цене (200 млн)
-        "property_type": "secondary",  # Фильтр для вторичной недвижимости
+        "only_flat": True,
+        "max_house_year": 2022,  # Ограничение по году постройки
+        "object_type": "secondary"  # Вторичка
     }
 )
 
